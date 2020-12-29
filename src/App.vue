9664b63a36f13a41c99d69180b44824f14094b35
Rizi Sadim <rizsad@jpj.com>
2020-12-29 22:11:39 +0800

    login for admin and employee

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <AdminLogin />
+    <EmployeeLogin />
+  </section>
+</template>
+
+<script>
+import AdminLogin from './components/AdminLogin.vue'
+import EmployeeLogin from './components/EmployeeLogin.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AdminLogin,
+    EmployeeLogin
+  }
+}
+</script>
